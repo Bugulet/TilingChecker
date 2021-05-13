043710d6103eb6b07db5257bc79a21a8ed9f7314
Eusebiu <Eusebiu@totalreality.nl>
2021-05-14 00:52:27 +0200

    Added image centering and zooming

--- sketch.js
+++ sketch.js
@@ -1,36 +1,53 @@
 
 let canvas;
+let imageSource=0;
+let imageScale=1.0;
+
+
 function setup() {
-    // create canvas
-    canvas = createCanvas(windowWidth,windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight);
+    
     background(100);
-    // Add an event for when a file is dropped onto the canvas
     canvas.drop(gotFile);
-  }
-  
-  function draw() {
-      background(100);
+}
+
+function draw() {
+    if(imageSource==0){
+    background(100);
     fill(255);
     noStroke();
     textSize(24);
     textAlign(CENTER);
     text('Drag an image file onto the canvas.', width / 2, height / 2);
-    noLoop();
-  }
-  
-  function gotFile(file) {
-    // If it's an image file
-    if (file.type === 'image') {
-      // Create an image DOM element but don't show it
-      const img = createImg(file.data).hide();
-      // Draw the image onto the canvas
-      image(img, 0, 0, width, height);
-    } else {
-      console.log('Not an image file!');
     }
-  }
+    else{
+        background(100);
+        imageMode(CENTER);
+        image(imageSource, width/2,height/2, imageSource.width*imageScale,imageSource.height*imageScale);
+    }
 
-  function windowResized() {
+}
+
+function gotFile(file) {
+    if (file.type === 'image') {
+        imageSource = createImg(file.data).hide();
+    } else {
+        console.log('Not an image file!');
+    }
+}
+
+function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseWheel(event) {
+    print(event.delta);
+    
+    if(event.delta>0){
+        imageScale-=0.1;
+    }
+    else{
+        imageScale+=0.1;
+    }
+    
   }
-  
